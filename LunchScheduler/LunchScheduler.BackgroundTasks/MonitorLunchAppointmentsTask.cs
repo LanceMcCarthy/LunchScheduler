9d357b0c7d53e04b55aa2c9cb5c0d45b3a28e049
Lance McCarthy <lance.mccarthy@telerik.com>
2016-12-19 17:08:32 -0500

    1.0.1 (added badge updater, tile updater and BGTask fixes)

--- LunchScheduler/LunchScheduler.BackgroundTasks/MonitorLunchAppointmentsTask.cs
+++ LunchScheduler/LunchScheduler.BackgroundTasks/MonitorLunchAppointmentsTask.cs
@@ -1,8 +1,33 @@
-﻿using System;
+﻿//  ---------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+// 
+//  The MIT License (MIT)
+// 
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+// 
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+// 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//  ---------------------------------------------------------------------------------
+
+using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
 using Windows.Storage;
 using Windows.UI.Notifications;
 using LunchScheduler.Data.Common;
@@ -21,7 +46,7 @@ namespace LunchScheduler.BackgroundTasks
         {
             var deferral = taskInstance.GetDeferral();
 
-            if(ApplicationData.Current.LocalSettings != null)
+            if (ApplicationData.Current.LocalSettings != null)
                 localSettings = ApplicationData.Current.LocalSettings;
 
             try
@@ -42,7 +67,7 @@ namespace LunchScheduler.BackgroundTasks
                 {
                     LogStatus($"{appointments.Count} Appointments Found");
                 }
-                
+
                 // Get the user's setting for how far ahead to check if there's an appointment
                 int monitorTimeWindow = 30;
                 object obj;
@@ -51,36 +76,31 @@ namespace LunchScheduler.BackgroundTasks
                     monitorTimeWindow = (int)obj;
                 }
 
+                int badgeCount = 0;
+
                 foreach (var lunch in appointments)
                 {
+                    var timeUntilAppointment = lunch.LunchTime - DateTimeOffset.Now;
+
                     // If the lunch appointment is within user's SelectedMonitorTimeWindow, create a notification
-                    if (DateTime.Now - lunch.LunchTime < TimeSpan.FromMinutes(monitorTimeWindow))
+                    if (timeUntilAppointment < TimeSpan.FromMinutes(monitorTimeWindow) && timeUntilAppointment > TimeSpan.Zero)
                     {
                         Debug.WriteLine($"Creating Toast for {lunch.Title}");
+                        badgeCount++;
 
-                        var visual = GenerateToastVisual(lunch);
+                        // create toast notification
+                        GenerateToastNotification(lunch);
+                        
+                        // Update Livetile / Lockscreen
+                        UpdateTile(lunch);
 
-                        var toastContent = new ToastContent
-                        {
-                            Visual = visual,
-                            // Toast Args. The app will navigate
-                            // to the LunchDetailPage when opened 
-                            // from a toast using the appointment ID
-                            Launch = $"?id={lunch.Id}"
-                        };
-
-                        var toast = new ToastNotification(toastContent.GetXml())
-                        {
-                            ExpirationTime = DateTime.Now.AddMinutes(30),
-                            Tag = $"{lunch.Id}",
-                            Group = "upcomingLunches"
-                        };
-
-                        ToastNotificationManager.CreateToastNotifier().Show(toast);
-
-                        LogStatus($"Sent Toast for {lunch.Title}");
+                        LogStatus($"Sent Toast and Tile for {lunch.Title}");
                     }
                 }
+
+                // Update the app tile/lockscreen badge count
+                if(badgeCount > 0)
+                    UpdateBadge(badgeCount);
 
                 LogStatus($"Successful, last run {DateTime.Now:U}");
             }
@@ -101,10 +121,11 @@ namespace LunchScheduler.BackgroundTasks
         /// <summary>
         /// Creates a ToastNotification Visual (text, image, etc.)
         /// </summary>
-        /// <param name="lunch">Appointment to create a visual for</param>
-        /// <returns>A visuial with either the message or a message and photo of a guest</returns>
-        private ToastVisual GenerateToastVisual(LunchAppointment lunch)
+        /// <param name="lunch">Appointment to create a notification for</param>
+        private void GenerateToastNotification(LunchAppointment lunch)
         {
+            ToastVisual visual = null;
+
             if (lunch.Guests.Count > 0)
             {
                 string message = $"You have lunch at {lunch.LunchTime:t} with";
@@ -112,8 +133,84 @@ namespace LunchScheduler.BackgroundTasks
                 for (int i = 0; i < lunch.Guests.Count - 1; i++)
                 {
                     // Add the proper punctuation and grammar between guest names
+                    if (i == 0)
+                        message += " "; // First guest
+                    else if (i == lunch.Guests.Count - 1)
+                        message += " and "; // Last guest
+                    else
+                        message += ", ";
 
-                    if (i == 0) 
+                    // Add the guest's name
+                    message += $"{lunch.Guests[i].FullName}";
+                }
+
+                // Limit the size of the string so that we stay under the 5kb payload limit
+                if (message.Length > 100)
+                    message = message.Substring(0, 99);
+
+                // Create and return a visual that has the guest's names and the first guest's photo
+                visual = new ToastVisual
+                {
+                    BindingGeneric = new ToastBindingGeneric()
+                    {
+                        Children =
+                        {
+                            new AdaptiveText { Text = "Lunch Time!" },
+                            new AdaptiveText { Text = message, HintWrap = true, HintStyle = AdaptiveTextStyle.Caption},
+                            new AdaptiveImage { Source = lunch.Guests[0].ThumbnailUri, HintCrop = AdaptiveImageCrop.Circle }
+                        }
+                    }
+                };
+            }
+            else
+            {
+                // If there are no guests, just use the location name
+                visual = new ToastVisual
+                {
+                    BindingGeneric = new ToastBindingGeneric
+                    {
+                        Children =
+                        {
+                            new AdaptiveText { Text = "Lunch Time!" },
+                            new AdaptiveText { Text = $"You have lunch at {lunch.LunchTime:t} at {lunch.Location.Name}", HintWrap = true }
+                        }
+                    }
+                };
+            }
+            
+            var toastContent = new ToastContent
+            {
+                Visual = visual,
+                // Toast Args. The app will navigate
+                // to the LunchDetailPage when opened 
+                // from a toast using the appointment ID
+                Launch = $"?id={lunch.Id}"
+            };
+
+            var toast = new ToastNotification(toastContent.GetXml())
+            {
+                ExpirationTime = DateTime.Now.AddMinutes(30),
+                Tag = $"{lunch.Id}"
+            };
+
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+
+        private void UpdateTile(LunchAppointment lunch)
+        {
+            TileVisual visual = null;
+
+            if (lunch.Guests.Count > 0)
+            {
+                // if there are guests build tile update with their images and names
+
+                string message = $"You have lunch at {lunch.LunchTime:t} with";
+
+                for (int i = 0; i < lunch.Guests.Count - 1; i++)
+                {
+                    // Add the proper punctuation and grammar between guest names
+
+                    if (i == 0)
                         message += ": "; // First guest
                     else if (i == lunch.Guests.Count - 1)
                         message += " and "; // Last guest
@@ -123,34 +220,145 @@ namespace LunchScheduler.BackgroundTasks
                     // Add the guest's name
                     message += $"{lunch.Guests[i].FullName}";
                 }
-                
+
                 // Create and return a visual that has the guest's names and the first guest's photo
-                return new ToastVisual
+                visual = new TileVisual
                 {
-                    BindingGeneric = new ToastBindingGeneric()
+                    TileMedium = new TileBinding
                     {
-                        Children =
+                        Content = new TileBindingContentAdaptive
                         {
-                            new AdaptiveText { Text = "Lunch Time!" },
-                            new AdaptiveText { Text = message, HintWrap = true },
-                            new AdaptiveImage { Source = lunch.Guests[0].ThumbnailUri, HintCrop = AdaptiveImageCrop.Circle }
+                            Children =
+                            {
+                                new AdaptiveImage {Source = lunch.Guests[0].ThumbnailUri, HintCrop = AdaptiveImageCrop.Circle},
+                                new AdaptiveText {Text = "Lunch Time!", HintStyle = AdaptiveTextStyle.Header},
+                                new AdaptiveText
+                                {
+                                    Text = message.Substring(0,62),
+                                    HintWrap = true,
+                                    HintStyle = AdaptiveTextStyle.Body
+                                }
+                            }
+                        }
+                    },
+                    TileWide = new TileBinding
+                    {
+                        Content = new TileBindingContentAdaptive
+                        {
+                            Children =
+                            {
+                                new AdaptiveImage {Source = lunch.Guests[0].ThumbnailUri, HintCrop = AdaptiveImageCrop.Circle},
+                                new AdaptiveText {Text = "Lunch Time!", HintStyle = AdaptiveTextStyle.Header},
+                                new AdaptiveText
+                                {
+                                    Text = message.Substring(0,133),
+                                    HintWrap = true,
+                                    HintStyle = AdaptiveTextStyle.Body
+                                }
+                            }
+                        }
+                    },
+                    TileLarge = new TileBinding
+                    {
+                        Content = new TileBindingContentAdaptive
+                        {
+                            Children =
+                            {
+                                new AdaptiveImage {Source = lunch.Guests[0].ThumbnailUri, HintCrop = AdaptiveImageCrop.Circle},
+                                new AdaptiveText {Text = "Lunch Time!", HintStyle = AdaptiveTextStyle.Header},
+                                new AdaptiveText
+                                {
+                                    Text =message.Substring(0,81),
+                                    HintWrap = true,
+                                    HintStyle = AdaptiveTextStyle.Body
+                                }
+                            }
                         }
                     }
                 };
             }
-
-            // If there are no guests, just use the location name
-            return new ToastVisual()
+            else
             {
-                BindingGeneric = new ToastBindingGeneric()
+                // No guests
+                visual = new TileVisual
                 {
-                    Children =
+                    TileMedium = new TileBinding
                     {
-                        new AdaptiveText { Text = "Lunch Time!" },
-                        new AdaptiveText { Text = $"You have lunch at {lunch.LunchTime:t} at {lunch.Location.Name}", HintWrap = true }
+                        Content = new TileBindingContentAdaptive
+                        {
+                            Children =
+                            {
+                                new AdaptiveText {Text = "Lunch Time!", HintStyle = AdaptiveTextStyle.Header},
+                                new AdaptiveText
+                                {
+                                    Text = $"You have lunch at {lunch.LunchTime:t} at {lunch.Location.Name}",
+                                    HintWrap = true,
+                                    HintStyle = AdaptiveTextStyle.Body
+                                }
+                            }
+                        }
+                    },
+                    TileWide = new TileBinding
+                    {
+                        Content = new TileBindingContentAdaptive
+                        {
+                            Children =
+                            {
+                                new AdaptiveText {Text = "Lunch Time!", HintStyle = AdaptiveTextStyle.Header},
+                                new AdaptiveText
+                                {
+                                    Text = $"You have lunch at {lunch.LunchTime:t} at {lunch.Location.Name}",
+                                    HintWrap = true,
+                                    HintStyle = AdaptiveTextStyle.Body
+                                }
+                            }
+                        }
+                    },
+                    TileLarge = new TileBinding
+                    {
+                        Content = new TileBindingContentAdaptive
+                        {
+                            Children =
+                            {
+                                new AdaptiveText {Text = "Lunch Time!", HintStyle = AdaptiveTextStyle.Header},
+                                new AdaptiveText
+                                {
+                                    Text = $"You have lunch at {lunch.LunchTime:t} at {lunch.Location.Name}",
+                                    HintWrap = true,
+                                    HintStyle = AdaptiveTextStyle.Body
+                                }
+                            }
+                        }
                     }
-                }
+                };
+            }
+            
+            var content = new TileContent()
+            {
+                Visual = visual
             };
+
+            var notification = new TileNotification(content.GetXml());
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(notification);
+        }
+
+        private void UpdateBadge(int num)
+        {
+            var badgeXml = BadgeUpdateManager.GetTemplateContent(BadgeTemplateType.BadgeNumber);
+            
+            var badgeElement = badgeXml.SelectSingleNode("/badge") as XmlElement;
+
+            if (badgeElement == null)
+                return;
+
+            badgeElement.SetAttribute("value", num.ToString());
+            
+            var badge = new BadgeNotification(badgeXml);
+            
+            var badgeUpdater = BadgeUpdateManager.CreateBadgeUpdaterForApplication();
+            
+            badgeUpdater.Update(badge);
+
         }
 
         /// <summary>
@@ -159,7 +367,7 @@ namespace LunchScheduler.BackgroundTasks
         /// <param name="message">Status message of operation</param>
         private void LogStatus(string message)
         {
-            if(localSettings!=null)
+            if (localSettings != null)
                 localSettings.Values[Constants.BackgroundTaskStatusSettingsKey] = message;
 
 #if DEBUG
